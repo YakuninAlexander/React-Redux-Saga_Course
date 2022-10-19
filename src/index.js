7bb8acc93bbf7ee6a22b5634a2416444424860ba
YakuninAlexander <wepqwppw@gmail.com>
2022-10-19 12:41:41 +0300

    ver 1.1 Добавлены саги

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
 import { compose, createStore, applyMiddleware } from 'redux';
 import App from './App';
 import { rootReducer } from './redux/rootReducer';
 import reportWebVitals from './reportWebVitals';
+import { forbiddenWordsMiddleware } from './redux/middleware';
+import { sagaWatcher } from './redux/sagas';
 
+const saga = createSagaMiddleware()
 
 const store = createStore(rootReducer,compose(
   applyMiddleware(
-    thunk
+    thunk, forbiddenWordsMiddleware, saga
   ),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
+
+saga.run(sagaWatcher);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const app = (
